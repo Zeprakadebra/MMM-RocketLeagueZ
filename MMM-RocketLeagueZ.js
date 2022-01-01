b506feb1ba1137258986c78fab98abc0d9f6e932
Oliver Petersen <oliver.k.petersen@googlemail.com>
2022-01-01 17:22:26 +0100

    changes

--- MMM-RocketLeagueZ.js
+++ MMM-RocketLeagueZ.js
@@ -8,7 +8,6 @@
 Module.register('MMM-RocketLeagueZ', {
 	// default configuration
 	defaults: {
-		header: 'Haleluja',
         baseURL: 'https://api.tracker.gg/api/v2/rocket-league/standard/profile/',
         gamers: [
             'xbl/SkillKiddo', 
@@ -101,62 +100,44 @@ Module.register('MMM-RocketLeagueZ', {
 			gamers.forEach((gamer) => {
 
 				let row = document.createElement('tr');
-					row.className = 'normal bright stats-row';
 
 				let cell = document.createElement('td');
 					cell.rowSpan = 2;
-					cell.innerHTML = "<img src='" + gamer.avatarURL + "' width=100px height=100px alt='" + gamer.tag + "'/>"  
+					cell.innerHTML = "<img src='" + gamer.avatarURL + "' width=80px height=80px alt='" + gamer.tag + "'/>"  
 					cell.style.cssText = 'text-align: left;';
 					row.appendChild(cell);
 			
 					cell = document.createElement('td');
 					cell.rowSpan = 2;
+					cell.className = 'gamer';
 					cell.innerHTML = gamer.tag;  
 					cell.style.cssText = 'text-align: left;';
 					row.appendChild(cell);
 
 					cell = document.createElement('td');
+					cell.className = 'mmr';
 					cell.innerHTML = gamer.rankValue;  
 					cell.style.cssText = 'text-align: right;';
 					row.appendChild(cell);
 
 					cell = document.createElement('td');
 					cell.rowSpan = 2;
-					cell.innerHTML = "<img src='" + gamer.iconURL + "' width=100px height=100px alt='" + gamer.rankName + "'/>"  
+					cell.innerHTML = "<img src='" + gamer.iconURL + "' width=80px height=80px alt='" + gamer.rankName + "'/>"  
 					cell.style.cssText = 'text-align: left;';
 					row.appendChild(cell);
 					wrapper.appendChild(row);
 
 					row = document.createElement('tr');
-					row.className = 'normal bright stats-row';
 
 					cell = document.createElement('td');
+					cell.className = 'division';
 					cell.innerHTML = gamer.divisionNumber;  
 					cell.style.cssText = 'text-align: right;';
 					row.appendChild(cell);
 					wrapper.appendChild(row);
-
-				console.log(gamer.tag + ': ' + gamer.rankName + '/' + gamer.divisionNumber + ' => ' + gamer.rankValue + ' = ' + gamer.avatarURL);
 			});
         }
 		return wrapper;
-	},
-
-	createRankTableCell: function(row, iconURL, rankName, divisionNumber, ratingValue, className, align)
-	{
-		const text = "<img src='" + iconURL + "' width=20px height=20px alt='" + rankName + "'/>" + new Intl.NumberFormat().format(divisionNumber) + "/" + new Intl.NumberFormat().format(ratingValue);
-		this.createTableCell(row, text, className, align);
-	},
-
-	createTableCell: function(row, text, className, align = 'left')
-	{
-		let cell = document.createElement('td');
-		cell.innerHTML = text;
-		cell.className = className;
-
-		cell.style.cssText = 'text-align: ' + align + ';';
-
-		row.appendChild(cell);
 	},
 
 	// Override start to init stuff.
